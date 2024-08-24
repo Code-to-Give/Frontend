14aeaa2fcb2370ebb547bac340e877bf1d8ad778
ang-rui-yan <angruiyan@gmail.com>
2024-08-24 20:15:56 +0800

    chore: clean

--- food-connections/src/pages/SignupPage/SignUpPage.js
+++ food-connections/src/pages/SignupPage/SignUpPage.js
@@ -16,7 +16,7 @@ function SignUpPage() {
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
+    e.preventDefault();
 
     if (
       !companyName ||
