f6fe723f2e8554e05ebbf9ddabc2f8ab5112778a
ang-rui-yan <angruiyan@gmail.com>
2024-08-25 21:45:13 +0800

    feat: added donor info method

--- food-connections/src/pages/HomePage/DonorHomePage.js
+++ food-connections/src/pages/HomePage/DonorHomePage.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./DonorHomePage.css";
 import LoggedInNavbar from "../../components/LoggedInNavBar/LoggedInNavBar";
 import Piechart from "../../components/Piechart/Piechart";
@@ -8,59 +8,61 @@ import DonorPendingDonations from "../../components/PendingDonations/DonorPendin
 import FormPage from "../../pages/FormPage/FormPage";
 
 function DonorHomePage() {
-    const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
-    const toggleFormPopup = () => {
-        setIsFormOpen(!isFormOpen);
-    };
+  const toggleFormPopup = () => {
+    setIsFormOpen(!isFormOpen);
+  };
 
-    return (
-        <div>
-            <LoggedInNavbar />
-            <div className="dashboard-container">
-                <header className="dashboard-header">
-                    <h1>Hello there, MBS</h1>
-                    <button className="open-form-button" onClick={toggleFormPopup}>
-                        Food to donate?
-                    </button>
-                </header>
-                <div className="dashboard-content">
-                    <div className="charts">
-                        <div className="chart-item">
-                            <h2>Gender</h2>
-                            <Piechart />
-                        </div>
-
-                        <div className="chart-item">
-                            <h2>Location</h2>
-                            <DonutChart />
-                        </div>
-
-                        <div className="chart-item">
-                            <h2>Age</h2>
-                            <Histogram />
-                        </div>
-
-                        <div className="chart-item">
-                            <h2>Organisation</h2>
-                            <DonutChart />
-                        </div>
-                    </div>
-                    <div className="pending-donations">
-                        <DonorPendingDonations />
-                    </div>
-                </div>
+  return (
+    <div>
+      <LoggedInNavbar />
+      <div className="dashboard-container">
+        <header className="dashboard-header">
+          <h1>Hello there, MBS</h1>
+          <button className="open-form-button" onClick={toggleFormPopup}>
+            Food to donate?
+          </button>
+        </header>
+        <div className="dashboard-content">
+          <div className="charts">
+            <div className="chart-item">
+              <h2>Gender</h2>
+              <Piechart />
             </div>
-            {isFormOpen && (
-                <div className="popup">
-                    <div className="popup-content">
-                        <span className="close" onClick={toggleFormPopup}>&times;</span>
-                        <FormPage />
-                    </div>
-                </div>
-            )}
+
+            <div className="chart-item">
+              <h2>Location</h2>
+              <DonutChart />
+            </div>
+
+            <div className="chart-item">
+              <h2>Age</h2>
+              <Histogram />
+            </div>
+
+            <div className="chart-item">
+              <h2>Organisation</h2>
+              <DonutChart />
+            </div>
+          </div>
+          <div className="pending-donations">
+            <DonorPendingDonations />
+          </div>
         </div>
-    );
+      </div>
+      {isFormOpen && (
+        <div className="popup">
+          <div className="popup-content">
+            <span className="close" onClick={toggleFormPopup}>
+              &times;
+            </span>
+            <FormPage />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default DonorHomePage;
