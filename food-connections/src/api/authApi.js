3cf0a71959894c758d09346e72739c1df3134997
ang-rui-yan <angruiyan@gmail.com>
2024-08-25 15:11:48 +0800

    feat: added get user jwt and details

--- food-connections/src/api/authApi.js
+++ food-connections/src/api/authApi.js
@@ -5,19 +5,20 @@ const API_BASE_URL = process.env.AUTH_API_URL || "http://localhost:8000/api";
 export const login = async (data) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/users/login`, data);
-    return response ? response.data : null;
+    localStorage.setItem("accessToken", response.access_token);
+    return response;
   } catch (error) {
     if (error.response) {
       // Request made and server responded with a status other than 2xx
-      console.error('Error response:', error.response);
+      console.error("Error response:", error.response);
       return null; // or handle the error response as needed
     } else if (error.request) {
       // Request was made but no response was received
-      console.error('Error request:', error.request);
+      console.error("Error request:", error.request);
       return null;
     } else {
       // Something happened while setting up the request
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
       return null;
     }
   }
@@ -26,20 +27,39 @@ export const login = async (data) => {
 export const register = async (data) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/users`, data);
-    return response.data; // Return data if the request is successful
+    return response;
   } catch (error) {
     if (error.response) {
       // Server responded with a status other than 2xx
-      console.error('Error response:', error.response.data);
+      console.error("Error response:", error.response.data);
       return null; // Handle error accordingly
     } else if (error.request) {
       // Request was made but no response was received
-      console.error('Error request:', error.request);
+      console.error("Error request:", error.request);
       return null;
     } else {
       // Something happened in setting up the request
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
       return null;
     }
+  }
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  try {
+    const response = await axios.get(`${API_BASE_URL}/users/me`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`, // Add the Bearer token here
+      },
+    });
+
+    console.log("User data:", response);
+    return response;
+  } catch (err) {
+    // Handle any errors
+    console.error("Error fetching user data:", err);
+    throw err;
   }
 };
